b7332de5d0093cd808f593a09cf2786ccaa54d1e
Mohammed Salah Aboshosha <95502907+MohammedS2lah@users.noreply.github.com>
2024-12-21 18:06:27 +0200

    Update and rename GND.v to 2-GND.v

---
+++ 3-Circuits/1-Combinational Logic/1-Basic Gates/2-GND.v
@@ -0,0 +1,9 @@
+//*Exams/m2014 q4i*
+//-----------------------------------------------------
+
+module top_module (
+    output out);
+    
+    assign out = 1'b0;
+
+endmodule
--- 3-Circuits/1-Combinational Logic/1-Basic Gates/GND.v
+++
@@ -1,8 +0,0 @@
-*GND*
------------------------------------------------------
-module top_module (
-    output out);
-    
-    assign out = 1'b0;
-
-endmodule
