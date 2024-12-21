dfb7614f9b644bb6ab44114adf2ae02c27070088
Mohammed Salah Aboshosha <95502907+MohammedS2lah@users.noreply.github.com>
2024-12-21 18:04:42 +0200

    Create NOR.v

---
+++ 3-Circuits/1-Combinational Logic/1-Basic Gates/NOR.v
@@ -0,0 +1,10 @@
+*NOR*
+-----------------------------------------------------
+module top_module (
+    input in1,
+    input in2,
+    output out);
+    
+    assign out = !(in1 | in2);
+
+endmodule
