9487508298809c39e29bb9e918dc4ca1dc1371fd
Mohammed Salah Aboshosha <95502907+MohammedS2lah@users.noreply.github.com>
2024-12-23 03:18:36 +0200

    Create 1- Half adder.v

---
+++ 3-Circuits/1-Combinational Logic/3-Arithmetic Circuits/1- Half adder.v
@@ -0,0 +1,14 @@
+// * Hadd *
+// -----------------------------------------------------
+
+module top_module( 
+    input a, b,
+    output cout, sum );
+    
+    //assign sum = a ^ b;
+    
+    //assign cout = a & b;
+    
+    assign {cout,sum} = a + b;
+
+endmodule
