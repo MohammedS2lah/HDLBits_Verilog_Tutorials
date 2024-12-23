7db5d6d7cdc0e9d9b5347c70e79f0cb399ecaf31
Mohammed Salah Aboshosha <95502907+MohammedS2lah@users.noreply.github.com>
2024-12-23 03:31:12 +0200

    Create 6- Karnaugh map1.v

---
+++ 3-Circuits/1-Combinational Logic/4-Karnaugh Map to Circuit/6- Karnaugh map1.v
@@ -0,0 +1,10 @@
+// * Exams/m2014 q3 *
+// -----------------------------------------------------
+
+module top_module (
+    input [4:1] x, 
+    output f );
+
+    assign f = (x[3] & !x[1]) | (x[4] & x[2]);
+    
+endmodule
