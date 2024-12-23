2a710a8948702bafe12c516c392ca24a04fdb3ac
Mohammed Salah Aboshosha <95502907+MohammedS2lah@users.noreply.github.com>
2024-12-23 03:19:20 +0200

    Create 2- Full adder.v

---
+++ 3-Circuits/1-Combinational Logic/3-Arithmetic Circuits/2- Full adder.v
@@ -0,0 +1,15 @@
+// * Fadd *
+// -----------------------------------------------------
+
+module top_module( 
+    input a, b, cin,
+    output cout, sum );
+    
+    //assign {cout, sum} = a + b + cin;
+    
+    assign sum = a ^ b ^ cin;
+    
+    assign cout = (a & b) | (a & cin) | (b & cin);
+    
+
+endmodule
