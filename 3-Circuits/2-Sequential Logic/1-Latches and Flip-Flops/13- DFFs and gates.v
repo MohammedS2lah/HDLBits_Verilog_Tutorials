8f15c56b36d3ecf9f733caa881ce16973abf8bdd
Mohammed Salah Aboshosha <95502907+MohammedS2lah@users.noreply.github.com>
2024-12-25 08:09:30 +0200

    Create 13- DFFs and gates.v

---
+++ 3-Circuits/2-Sequential Logic/1-Latches and Flip-Flops/13- DFFs and gates.v
@@ -0,0 +1,23 @@
+// * Exams/ece241 2014 q4 *
+// -----------------------------------------------------
+
+module top_module (
+    input clk,
+    input x,
+    output z
+); 
+    
+    wire q1, q2, q3;
+    
+    always @(posedge clk)
+        begin
+            q1 <= x ^ q1;
+            
+            q2 <= x & !q2;
+            
+            q3 <= x | !q3;
+        end
+    
+    assign z = !(q1 | q2 | q3);
+
+endmodule
