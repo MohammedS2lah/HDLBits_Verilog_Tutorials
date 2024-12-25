9bae1ebedec35ccee2834f1d1cff59498a535d40
Mohammed Salah Aboshosha <95502907+MohammedS2lah@users.noreply.github.com>
2024-12-25 08:11:50 +0200

    Create 16- Detect both edges.v

---
+++ 3-Circuits/2-Sequential Logic/1-Latches and Flip-Flops/16- Detect both edges.v
@@ -0,0 +1,18 @@
+// * Edgedetect2 *
+// -----------------------------------------------------
+
+module top_module (
+    input clk,
+    input [7:0] in,
+    output [7:0] anyedge
+);
+    
+    reg [7:0] in_reg;
+    
+    always @(posedge clk)
+        begin
+            in_reg <= in;
+            anyedge <= in_reg ^ in;
+        end    
+    
+endmodule
