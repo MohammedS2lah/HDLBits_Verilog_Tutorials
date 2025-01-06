b85601aa7e77f77deefa2f78c37261bfe8e2f525
Mohammed Salah Aboshosha <95502907+MohammedS2lah@users.noreply.github.com>
2025-01-06 12:51:22 +0200

    Update 1- Counter with period 1000.v

--- 3-Circuits/3-Building Larger Circuits/1- Counter with period 1000.v
+++ 3-Circuits/3-Building Larger Circuits/1- Counter with period 1000.v
@@ -1,1 +1,16 @@
 // * Exams/review2015 count1k *
+// -----------------------------------------------------
+
+module top_module (
+    input clk,
+    input reset,
+    output [9:0] q);
+    
+    always @(posedge clk)
+        begin
+            if (reset)	q <= 'b0;
+            else if (q == 999)	q <= 'b0;
+            else q <= q + 1'b1;    
+        end
+    
+endmodule
