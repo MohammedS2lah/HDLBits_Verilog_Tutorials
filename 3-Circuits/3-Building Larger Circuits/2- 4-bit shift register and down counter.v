75f735b7cd39293f7298929946f5030b5a921065
Mohammed Salah Aboshosha <95502907+MohammedS2lah@users.noreply.github.com>
2025-01-06 12:51:46 +0200

    Update 2- 4-bit shift register and down counter.v

--- 3-Circuits/3-Building Larger Circuits/2- 4-bit shift register and down counter.v
+++ 3-Circuits/3-Building Larger Circuits/2- 4-bit shift register and down counter.v
@@ -1,1 +1,27 @@
 // * Exams/review2015 shiftcount *
+// -----------------------------------------------------
+
+module top_module (
+    input clk,
+    input shift_ena,
+    input count_ena,
+    input data,
+    output [3:0] q);
+    
+    always @(posedge clk)
+        begin
+            if (shift_ena) 
+                begin
+                    /*
+                    q[0] <= data;
+                    q[1] <= q[0];
+                    q[2] <= q[1];
+                    q[3] <= q[2];
+                    */
+                    q <= {q[2:0],data};
+                end
+            else if (count_ena)
+            	q <= q - 1'b1;
+        end
+
+endmodule
