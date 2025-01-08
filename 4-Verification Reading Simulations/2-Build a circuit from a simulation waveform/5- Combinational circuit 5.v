0a9d56f2d419ed2a3fbc511651ea2fc3384db48b
Mohammed Salah Aboshosha <95502907+MohammedS2lah@users.noreply.github.com>
2025-01-08 10:20:17 +0200

    Update 5- Combinational circuit 5.v

--- 4-Verification Reading Simulations/2-Build a circuit from a simulation waveform/5- Combinational circuit 5.v
+++ 4-Verification Reading Simulations/2-Build a circuit from a simulation waveform/5- Combinational circuit 5.v
@@ -13,7 +13,7 @@ module top_module (
         begin
             case (c)
                 0: q = b;
-				1: q = e;
+		1: q = e;
                 2: q = a;
                 3: q = d;
                 default: q = 4'hf;
