ed5687f7102ec704d19c98e642683efb02807c0d
Mohammed Salah Aboshosha <95502907+MohammedS2lah@users.noreply.github.com>
2025-01-08 10:25:29 +0200

    Create 10- Sequential circuit 10.v

---
+++ 4-Verification Reading Simulations/2-Build a circuit from a simulation waveform/10- Sequential circuit 10.v
@@ -0,0 +1,31 @@
+// * Sim/circuit10 *
+// -----------------------------------------------------
+
+module top_module (
+    input clk,
+    input a,
+    input b,
+    output q,
+    output state  );
+    
+    
+    always @(posedge clk)
+        begin
+            if (a == b) state <= a;
+            else state <= state;
+        end
+    
+    always @(*) q = a ^ b ^ state; 
+    
+    /*
+   	// Another solution
+    always @(posedge clk)
+        begin
+            if (a == b) state <= a;
+            else state <= state;
+        end    
+    
+    assign q = (a == b) ? state : ~state;
+    */
+
+endmodule
