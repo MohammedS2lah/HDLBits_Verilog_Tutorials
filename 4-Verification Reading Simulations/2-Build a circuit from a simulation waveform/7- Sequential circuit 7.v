5a572cc50033d7c62fd7ebeec62b655beed9de03
Mohammed Salah Aboshosha <95502907+MohammedS2lah@users.noreply.github.com>
2025-01-08 10:21:36 +0200

    Create 7- Sequential circuit 7.v

---
+++ 4-Verification Reading Simulations/2-Build a circuit from a simulation waveform/7- Sequential circuit 7.v
@@ -0,0 +1,11 @@
+// * Sim/circuit7 *
+// -----------------------------------------------------
+
+module top_module (
+    input clk,
+    input a,
+    output q );
+
+    always @(posedge clk) q <= ~a;
+    
+endmodule
